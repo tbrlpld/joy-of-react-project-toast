192e2f2dff14439a9fb1fdd2fa133a62869ea24c
Tibor Leupold <tibor@lpld.io>
2025-08-14 13:58:00 -0700

    Use icon based on variant

--- src/components/Toast/Toast.js
+++ src/components/Toast/Toast.js
@@ -19,10 +19,12 @@ const ICONS_BY_VARIANT = {
 };
 
 function Toast({variant='notice', onDismiss, children}) {
+  const IconTag = ICONS_BY_VARIANT[variant]
+
   return (
     <div className={`${styles.toast} ${styles[variant]}`}>
       <div className={styles.iconContainer}>
-        <Info size={24} />
+        <IconTag size={24} />
       </div>
 
       <p className={styles.content}>{children}</p>
