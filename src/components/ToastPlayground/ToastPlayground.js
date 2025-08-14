2689276dd3a8fe2d0fb6311dfd339f604be5b663
Tibor Leupold <tibor@lpld.io>
2025-08-14 13:07:06 -0700

    Make single radio controlled

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -9,6 +9,8 @@ const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 function ToastPlayground() {
   const [message, setMessage] = React.useState('')
 
+  const [variant, setVariant] = React.useState(VARIANT_OPTIONS[0])
+
   return (
     <div className={styles.wrapper}>
       <header>
@@ -32,18 +34,11 @@ function ToastPlayground() {
 
         <div className={styles.row}>
           <div className={styles.label}>Variant</div>
+
           <div
             className={`${styles.inputWrapper} ${styles.radioWrapper}`}
           >
-            <label htmlFor="variant-notice">
-              <input
-                id="variant-notice"
-                type="radio"
-                name="variant"
-                value="notice"
-              />
-              notice
-            </label>
+            <Radio name={"notice"} label={"Notice"} value={"notice"} checkedValue={variant} setCheckedValue={setVariant}/>
 
             {/* TODO Other Variant radio buttons here */}
           </div>
@@ -60,6 +55,24 @@ function ToastPlayground() {
       </div>
     </div>
   );
+}
+
+function Radio ({name, label, value, checkedValue, setCheckedValue}) {
+  const id = React.useId()
+
+  return (
+    <label htmlFor={id}>
+      <input
+        id={id}
+        type="radio"
+        name={name}
+        value={value}
+        checked={value === checkedValue}
+        onChange={() => setCheckedValue(value)}
+      />
+      {label}
+    </label>
+  )
 }
 
 export default ToastPlayground;
