a3f952127cb3c3df2a134f3146103e74cb5bfb48
Tibor Leupold <tibor@lpld.io>
2025-08-14 12:50:04 -0700

    Control message

--- src/components/ToastPlayground/ToastPlayground.js
+++ src/components/ToastPlayground/ToastPlayground.js
@@ -7,6 +7,8 @@ import styles from './ToastPlayground.module.css';
 const VARIANT_OPTIONS = ['notice', 'warning', 'success', 'error'];
 
 function ToastPlayground() {
+  const [message, setMessage] = React.useState('')
+
   return (
     <div className={styles.wrapper}>
       <header>
@@ -24,7 +26,9 @@ function ToastPlayground() {
             Message
           </label>
           <div className={styles.inputWrapper}>
-            <textarea id="message" className={styles.messageInput} />
+            <textarea id="message" className={styles.messageInput} onChange={(event) => setMessage(event.target.value)}>
+              {message}
+            </textarea>
           </div>
         </div>
 
