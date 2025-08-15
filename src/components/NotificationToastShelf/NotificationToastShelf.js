fee8cc4cd2bd035bc243a4f58a9a655eb06d34cd
Tibor Leupold <tibor@lpld.io>
2025-08-15 09:06:20 -0700

    Dismiss all notification toasts via Escape

--- src/components/NotificationToastShelf/NotificationToastShelf.js
+++ src/components/NotificationToastShelf/NotificationToastShelf.js
@@ -1,11 +1,15 @@
 import React from 'react';
 
 import { NotificationContext } from '../NotificationProvider'
+import useEscape from '../../hooks/use-escape'
 import ToastShelf from '../ToastShelf'
 import NotificationToast from '../NotificationToast'
 
+
 function NotificationToastShelf() {
-  const { notifications } = React.useContext(NotificationContext)
+  const { notifications, dismissAllNotifications } = React.useContext(NotificationContext)
+
+  useEscape({callback: dismissAllNotifications})
 
   return (
     <ToastShelf>
